08babd29717ed0bbdca06a7d1744660ab94b4c44
newstar94 <77623864+newstar94@users.noreply.github.com>
2021-07-02 09:36:21 +0700

    BaiJS2

---
+++ js/js2.js
@@ -0,0 +1,35 @@
+var selectednumber=[];
+function confirm(){
+    var input = document.getElementById("number");
+    var num = parseInt(input.value);
+    if(!selectednumber.includes(num)&&selectednumber.length<5&&num>=0&&num<=99){
+        selectednumber.push(num);
+        // alert("Ban da lua chon so: "+num);
+    }
+}
+function test(){
+    var luckynumber=[];
+    while(luckynumber.length<5){
+        var n = parseInt(Math.random()*99+1);
+        if(!luckynumber.includes(n)){
+            luckynumber.push(n);
+        }
+    }
+    var kq=document.getElementById('kq');
+    kq.innerHTML=luckynumber;
+    console.log(selectednumber);
+    if(selectednumber.length==5){
+        var count = 0;
+        for(var i=0;i<5;i++){
+            if(luckynumber.includes(selectednumber[i])){
+                count++;
+            }
+        }
+        switch (count){
+            case 3: alert("Chuc mung ban da trung thuong 500.000VND");break;
+            case 4: alert("Chuc mung ban da trung thuong 20.000.000VND");break;
+            case 5: alert("Chuc mung ban da trung thuong 10.000.000.000VND");break;
+            default: alert("Rat tiec, chuc ban may man lan sau.");
+        }
+    }
+}
