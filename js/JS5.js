703e6a4c9608f5d2205d9436636325f84b31fea6
newstar94 <77623864+newstar94@users.noreply.github.com>
2021-07-09 16:37:03 +0700

    BaiJS5

--- js/JS5.js
+++ js/JS5.js
@@ -14,7 +14,19 @@ $.ajax({
         hum.innerHTML="Độ ẩm: "+rs.list[0].main.humidity + "%";
         wind.innerHTML="Gió: "+rs.list[0].wind.speed + " km/h";
         press.innerHTML="Áp suất: "+rs.list[0].main.pressure +" atm";
-        temp.innerHTML=rs.list[0].main.temp;
-        icon.src = "https://openweathermap.org/img/w/" + rs.list[0].weather[0].icon + ".png"
+        temp.innerHTML=parseInt(rs.list[0].main.temp);
+        iconmain.src = "https://openweathermap.org/img/w/" + rs.list[0].weather[0].icon + ".png"
+        var p =[];
+        p = rs.list;
+        var row = $("#row");
+        for (var i=0;i<40;i=i+8){
+            var ct = "<div class=\"col-md-2\">\n" +
+                "                <img src=\""+"https://openweathermap.org/img/w/" +p[i].weather[0].icon + ".png"+"\" width=\"100%\">\n" +
+                "                <span class=\"min\">"+p[i].main.temp_min+"</span>\n" +
+                "                <span class=\"max\">"+p[i].main.temp_max+"</span>\n" +
+                "            </div>";
+            row.append(ct);
+        }
+
     }
 })
